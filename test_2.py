edc048b4d8cd8ab6e3de1b08d42853b02c475af9
Raven <raven@Ravens-MacBook-Air.local>
2020-12-04 09:43:00 +0800

    finish pseudo-code

--- test_2.py
+++ test_2.py
@@ -21,4 +21,29 @@ Print out the calculated value to the assigned variable.
 """
 import final.txt
 main():
-  
+  set dictionary = create_dictionary ("final.txt")
+  num_grades = formListofGrades (dictionary)
+  avg_grade = createAverageGrade (num_grades)
+  calculate_percent_above_average = createPercentAboveAverage (avg_grade)
+
+def create_dictionary("final.txt"):
+  read in final.txt
+  create list = each line from the text file
+  close the file
+  create dict off the list
+  return dict
+
+def num_grades(dictionary):
+    calculate the total number
+    print number of grades
+
+def avg_grade(num_grades):
+    calculate avg = total grade / number of grades
+    print avg grade
+
+def calculate_percent_above_average(avg_grade):
+    calculate percentage = (total grades > avg) / number of grades
+    print percentage grade above the avg 
+
+main()
+"""
