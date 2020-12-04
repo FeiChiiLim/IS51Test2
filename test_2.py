9bd51e0d7420eedc2e6c19f2640aecce761091a2
Raven <raven@Ravens-MacBook-Air.local>
2020-12-04 09:01:16 +0800

    finish structured-english

---
+++ test_2.py
@@ -0,0 +1,18 @@
+"""
+The program is trying to display the number of grades, the average grade, 
+and the percentage of grades that are above the average grade in a final exam.
+We first read in text file final.txt
+then from the text file, we create a list of integer from each lines in the text file
+Then, we create a dictionary from the list
+Once the text file has been read into memory, we close the file.
+
+The program will read the input text file to assign the grades to a list of integer.
+
+We then define a function that calculate the number of grades
+then, calculate the average grade which take the total grade / number of grades
+we convert the output of average grade to float
+then, we define a function that calculate the percentage of grades that are above the average grade
+we convert the output of the  percentage of grades that are above the average grade to float and add percentage
+
+Print out the calculated value to the assigned variable.
+"""
