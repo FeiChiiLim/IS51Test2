eeac38e1f3bfe4ce32a69f222ec21bff6457358c
Raven <raven@Ravens-MacBook-Air.local>
2020-12-04 09:17:24 +0800

    finish psedocode

--- test_2.py
+++ test_2.py
@@ -2,10 +2,10 @@
 The program is trying to display the number of grades, the average grade, 
 and the percentage of grades that are above the average grade in a final exam.
 We first read in text file final.txt
-then from the text file, we create a list of integer from each lines in the text file
-Then, we create a dictionary from the list
+then from the text file, we create a list of integer from each lines of the text file
+We create a dictionary from the list.
 Once the text file has been read into memory, we close the file.
-
+The grades in text file will be imported to the program.
 The program will read the input text file to assign the grades to a list of integer.
 
 We then define a function that calculate the number of grades
@@ -16,3 +16,9 @@ we convert the output of the  percentage of grades that are above the average gr
 
 Print out the calculated value to the assigned variable.
 """
+
+
+"""
+import final.txt
+main():
+  
